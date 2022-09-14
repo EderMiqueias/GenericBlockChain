a7ae8532a041a93b59a41a1621fbf54985d6d7c4
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2022-09-13 23:52:23 -0300

    ajuste na declaraçãos dos metodos

--- blockchain.py
+++ blockchain.py
@@ -2,8 +2,6 @@ from datetime import datetime as dt
 import hashlib
 import json
 
-from flask import Flask, jsonify
-
 class Blockchain:
     def __init__(self) -> None:
         self.chain = list()
@@ -13,7 +11,8 @@ class Blockchain:
         block = {
             'index': len(self.chain) + 1,
             'timestamp': str(dt.now()),
-            'previous_hash': previous_hash
+            'previous_hash': previous_hash,
+            'proof': proof
         }
         self.chain.append(block)
         return block
@@ -21,7 +20,7 @@ class Blockchain:
     def get_previous_block(self):
         return self.chain[-1]
     
-    def get_hash_operation(new_proof, previous_proof):
+    def get_hash_operation(self, new_proof, previous_proof):
         return hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
     
     def proof_of_work(self, previous_proof):
@@ -36,9 +35,9 @@ class Blockchain:
         encoded_block = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
     
-    def is_chain_valid(self, chain):
-        previous_block = chain[0]
-        for block in chain:
+    def is_chain_valid(self):
+        previous_block = self.chain[0]
+        for block in self.chain:
             if block['previous_hash'] != self.hash(previous_block):
                 return False
             
